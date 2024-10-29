7f0d8a21105858b8b9d412c7016f05d25ef82bac
Eduardo García-Portugués <egarpor@users.noreply.github.com>
2024-10-29 09:24:16 +0100

    Organización del código

--- bike-WDC.R
+++ bike-WDC.R
@@ -1,124 +1,123 @@
-library(fda.usc) #better install from GitHub.  remotes::install_github("moviedo5/fda.usc")
-# Lectura de datos
-data=read.csv("./hour.csv")
-l=which(data$weekday==6)
-datan=data[l,]
-tab=table(datan$dteday,datan$hr)
-dayf=unique(rownames(which(tab==0,arr.ind=TRUE)))
-l=which(data$weekday==6 & !(data$dteday %in% dayf))
-datan=data[l,]
-ddays=unique(datan$dteday)
-#lesp=which(ddays %in% c("2011-09-17","2012-01-21","2012-06-09"))
- tmin=-8; tmax=39
- atmin=-16; atmax=50
- fhum=100
- fws=67
-# Conversión a datos funcionales
-tj=seq(0.5,23.5)
-y0=fdata(matrix(datan$casual,ncol=24,byrow=TRUE),tj,c(0,24),
-		names=list(main="NBCR",xlab="Hora",ylab="NBCR"))
-nbcr=apply(y0$data,1,sum)
-Temp=fdata(matrix(tmin+datan$temp*(tmax-tmin),ncol=24,byrow=TRUE),tj,c(0,24),
-		names=list(main="Temperatura",xlab="Hora",ylab="ºC"))
-Hum=fdata(matrix(fhum*datan$hum,ncol=24,byrow=TRUE),tj,c(0,24),
-		names=list(main="Humedad",xlab="Hora",ylab=""))
-WS=fdata(matrix(fws*datan$windspeed,ncol=24,byrow=TRUE),tj,c(0,24),
-		names=list(main="Vel. Viento",xlab="Hora",ylab=""))
-FT=fdata(matrix(atmin+datan$atemp*(atmax-atmin),ncol=24,byrow=TRUE),tj,c(0,24),
-		names=list(main="Sensación térmica",xlab="Hora",ylab="ºC"))
-ldatm=ldata(df=data.frame(nbcr=nbcr,lnbcr=log(nbcr)),y=y0,Temp=Temp,Hum=Hum,WS=WS,FT=FT)
 
+# Instalar mejor desde GitHub: remotes::install_github("moviedo5/fda.usc")
+library(fda.usc)
+
+# Lectura de datos
+data <- read.csv("./hour.csv")
+l <- which(data$weekday == 6)
+datan <- data[l, ]
+tab <- table(datan$dteday, datan$hr)
+dayf <- unique(rownames(which(tab == 0, arr.ind = TRUE)))
+l <- which(data$weekday == 6 & !(data$dteday %in% dayf))
+datan <- data[l, ]
+ddays <- unique(datan$dteday)
+tmin <- -8
+tmax <- 39
+atmin <- -16
+atmax <- 50
+fhum <- 100
+fws <- 67
+
+# Conversión a datos funcionales
+tj <- seq(0.5, 23.5)
+y0 <- fdata(mdata = matrix(datan$casual, ncol = 24, byrow = TRUE),
+            argvals = tj, rangeval = c(0, 24),
+            names = list(main = "NBCR", xlab = "Hora", ylab = "NBCR"))
+nbcr <- apply(y0$data, 1, sum)
+Temp <- fdata(mdata = matrix(tmin + datan$temp * (tmax - tmin), ncol = 24,
+                             byrow = TRUE),
+              argvals = tj, rangeval = c(0, 24),
+              names = list(main = "Temperatura", xlab = "Hora", ylab = "ºC"))
+Hum <- fdata(mdata = matrix(fhum * datan$hum, ncol = 24, byrow = TRUE),
+             argvals = tj, rangeval = c(0, 24),
+             names = list(main = "Humedad", xlab = "Hora", ylab = ""))
+WS <- fdata(mdata = matrix(fws * datan$windspeed, ncol = 24, byrow = TRUE),
+            argvals = tj, rangeval = c(0, 24),
+            names = list(main = "Vel. Viento", xlab = "Hora", ylab = ""))
+FT <- fdata(mdata = matrix(atmin + datan$atemp * (atmax - atmin), ncol = 24,
+                           byrow = TRUE), argvals = tj, rangeval = c(0, 24),
+            names = list(main = "Sensación térmica", xlab = "Hora",
+                         ylab = "ºC"))
+ldatm <- ldata(df = data.frame(nbcr = nbcr, lnbcr = log(nbcr)), y = y0,
+               Temp = Temp, Hum = Hum, WS = WS, FT = FT)
+
+# Exploración datos (Figura 1)
 source("color.bar.R")
-pdf(file="bikerental.pdf",width=10.67,height=6)
-ncolor=cut(log(nbcr),5)
-colores=colorRampPalette(c("blue","orange"))(length(levels(ncolor)))
-#par(mfrow=c(2,2))
-par(mfrow=c(1,2))
-#plot(Temp,col=colores,ylim=c(0,50),lwd=2)
-#color.bar(colores,min(log(nbcr)),max(log(nbcr)))
-plot(Hum,col=colores,ylim=c(0,105),lwd=2)
-color.bar(colores,min(log(nbcr)),max(log(nbcr)),xpos=0)
-#plot(WS,col=colores,ylim=c(0,60),lwd=2)
-#color.bar(colores,min(log(nbcr)),max(log(nbcr)))
-plot(FT,col=colores,ylim=c(-10,50),lwd=2)
-color.bar(colores,min(log(nbcr)),max(log(nbcr)))
+pdf(file = "bikerental.pdf", width = 10.67, height = 6)
+ncolor <- cut(log(nbcr), 5)
+cols <- colorRampPalette(c("blue", "orange"))(length(levels(ncolor)))
+par(mfrow = c(1, 2))
+plot(Hum, col = cols, ylim = c(0, 105), lwd = 2)
+color.bar(cols, min(log(nbcr)), max(log(nbcr)), xpos = 0)
+plot(FT, col = cols, ylim = c(-10, 50), lwd = 2)
+color.bar(cols, min(log(nbcr)), max(log(nbcr)))
 dev.off()
 
-# base de componentes principales
-b.x=list(Temp=create.pc.basis(ldatm$Temp,1:6),Hum=create.pc.basis(ldatm$Hum,1:3),
-		WS=create.pc.basis(ldatm$WS,1:6),FT=create.pc.basis(ldatm$FT,1:6))
+# Base de componentes principales (truncamiento elegido según SICc,
+# ver test.Hum$p y test.FT$p)
+b.x <- list(
+  Temp = create.pc.basis(fdataobj = ldatm$Temp, l = 1:6),
+  Hum = create.pc.basis(fdataobj = ldatm$Hum, l = 1:3),
+  WS = create.pc.basis(fdataobj = ldatm$WS, l = 1:6),
+  FT = create.pc.basis(fdataobj = ldatm$FT, l = 1:6)
+)
 
-#rTemp=fregre.lm(lnbcr~Temp,data=ldatm,basis.x=b.x)
-rHum=fregre.lm(lnbcr~Hum,data=ldatm,basis.x=b.x)
-#rWS=fregre.lm(lnbcr~WS,data=ldatm,basis.x=b.x)
-rFT=fregre.lm(lnbcr~FT,data=ldatm,basis.x=b.x)
-
+# Regresiones y diagnósticos
+rHum <- fregre.lm(lnbcr ~ Hum, data = ldatm, basis.x = b.x)
+rFT <- fregre.lm(lnbcr ~ FT, data = ldatm, basis.x = b.x)
 summary(rHum)
 summary(rFT)
 
-pdf(file="FTregre.pdf",width=10.67,height=6)
-par(mfrow=c(1,2))
-plot(rHum,which=1,main="Humedad")
-plot(rFT,which=1,main="Sensación térmica")
+pdf(file = "FTregre.pdf", width = 10.67, height = 6)
+par(mfrow = c(1, 2))
+plot(rHum, which = 1, main = "Humedad")
+plot(rFT, which = 1, main = "Sensación térmica")
 dev.off()
 
-out.Hum=rp.flm.test(Hum,log(nbcr),B=2000)
-out.Hum$p.values.fdr
-out.FT=rp.flm.test(FT,log(nbcr),B=2000)
-out.FT$p.values.fdr
-#out.WS=rp.flm.test(WS,log(nbcr),B=2000)
+# Tests
+test.Hum <- rp.flm.test(Hum, log(nbcr), B = 2000)
+test.Hum$p.values.fdr
+test.Hum$p
+test.FT <- rp.flm.test(FT, log(nbcr), B = 2000)
+test.FT$p.values.fdr
+test.FT$p
 
-#CvM projections Hum
-par(mfrow=c(2,5))
+# CvM test Hum
+par(mfrow = c(2, 5))
 for (i in 1:10) {
-	rr=range(c(out.Hum$boot.proj.statistics[i,1,],out.Hum$proj.statistics[i,1]))
-	plot(density(out.Hum$boot.proj.statistics[i,1,]),main=paste0("Hum/CvM - R.P.:",i),xlim=rr)
-	abline(v=out.Hum$proj.statistics[i,1],col="red")
-	}
+  rr <- range(c(test.Hum$boot.proj.statistics[i, 1, ],
+                test.Hum$proj.statistics[i, 1]))
+  plot(density(test.Hum$boot.proj.statistics[i, 1, ]),
+       main = paste0("Hum/CvM - R.P.:", i), xlim = rr)
+  abline(v = test.Hum$proj.statistics[i, 1], col = "red")
+}
 
-#KS projections FT
-par(mfrow=c(2,5))
+# KS test Hum
+par(mfrow = c(2, 5))
 for (i in 1:10) {
-	rr=range(c(out.Hum$boot.proj.statistics[i,2,],out.Hum$proj.statistics[i,2]))
-	plot(density(out.Hum$boot.proj.statistics[i,2,]),main=paste0("Hum/KS - R.P.:",i),xlim=rr)
-	abline(v=out.Hum$proj.statistics[i,2],col="red")
-	}
+  rr <- range(c(test.Hum$boot.proj.statistics[i, 2, ],
+                test.Hum$proj.statistics[i, 2]))
+  plot(density(test.Hum$boot.proj.statistics[i, 2, ]),
+       main = paste0("Hum/KS - R.P.:", i), xlim = rr)
+  abline(v = test.Hum$proj.statistics[i, 2], col = "red")
+}
 
-#CvM projections FT
-par(mfrow=c(2,5))
+# CvM test FT
+par(mfrow = c(2, 5))
 for (i in 1:10) {
-	rr=range(c(out.FT$boot.proj.statistics[i,1,],out.FT$proj.statistics[i,1]))
-	plot(density(out.FT$boot.proj.statistics[i,1,]),main=paste0("FT/CvM - R.P.:",i),xlim=rr)
-	abline(v=out.FT$proj.statistics[i,1],col="red")
-	}
+  rr <- range(c(test.FT$boot.proj.statistics[i, 1, ],
+                test.FT$proj.statistics[i, 1]))
+  plot(density(test.FT$boot.proj.statistics[i, 1, ]),
+       main = paste0("FT/CvM - R.P.:", i), xlim = rr)
+  abline(v = test.FT$proj.statistics[i, 1], col = "red")
+}
 
-#KS projections FT
-par(mfrow=c(2,5))
+# KS test FT
+par(mfrow = c(2, 5))
 for (i in 1:10) {
-	rr=range(c(out.FT$boot.proj.statistics[i,2,],out.FT$proj.statistics[i,2]))
-	plot(density(out.FT$boot.proj.statistics[i,2,]),main=paste0("FT/KS - R.P.:",i),xlim=rr)
-	abline(v=out.FT$proj.statistics[i,2],col="red")
-	}
-
-
-# Modelo final
-
-rmod=fregre.gsam(lnbcr~Hum+s(FT),data=ldatm,basis.x=b.x)
-summary(rmod)
-
-# Diagnósticos gráficos
-par(mfrow=c(1,3))
-plot(rmod$y~rmod$fitted.values,xlab="Fitted values",ylab="Response",main="log(NBCR)~Hum+f(ST)",
-sub=paste0(	"R^2=",round(1 - var((as.numeric(rmod$y) - rmod$fitted.values)) *
-        (length(rmod$y) - 1)/(var((as.numeric(rmod$y) - mean(rmod$y))) *
-        rmod$df.residual),3)))
-
-plot(rmod$residuals~rmod$fitted.values,xlab="Fitted values",ylab="Residuals",main="log(NBCR)~Hum+f(ST)")
-lines(lowess(rmod$fitted.values,rmod$residuals),lwd=2,col="red")
-
-yl <- as.expression(substitute(sqrt(abs(YL)), list(YL = "Std. resid.")))
-resid.sd=abs(rmod$residuals)/sd(rmod$residuals)
-plot(sqrt(resid.sd)~rmod$fitted.values,xlab="Fitted values",ylab=yl,main="log(NBCR)~Hum+f(ST)",sub="Scale-Location")
-lines(lowess(rmod$fitted.values,sqrt(resid.sd)),lwd=2,col="red")
-
-
+  rr <- range(c(test.FT$boot.proj.statistics[i, 2, ],
+                test.FT$proj.statistics[i, 2]))
+  plot(density(test.FT$boot.proj.statistics[i, 2, ]),
+       main = paste0("FT/KS - R.P.:", i), xlim = rr)
+  abline(v = test.FT$proj.statistics[i, 2], col = "red")
+}
--- color.bar.R
+++ color.bar.R
@@ -1,71 +1,95 @@
-image.scale <- function(z, zlim, col = heat.colors(12),
-breaks, horiz=TRUE, ylim=NULL, xlim=NULL, ...){
- if(!missing(breaks)){
-  if(length(breaks) != (length(col)+1)){stop("must have one more break than colour")}
- }
- if(missing(breaks) & !missing(zlim)){
-  breaks <- seq(zlim[1], zlim[2], length.out=(length(col)+1)) 
- }
- if(missing(breaks) & missing(zlim)){
-  zlim <- range(z, na.rm=TRUE)
-  zlim[2] <- zlim[2]+c(zlim[2]-zlim[1])*(1E-3)#adds a bit to the range in both directions
-  zlim[1] <- zlim[1]-c(zlim[2]-zlim[1])*(1E-3)
-  breaks <- seq(zlim[1], zlim[2], length.out=(length(col)+1))
- }
- poly <- vector(mode="list", length(col))
- for(i in seq(poly)){
-  poly[[i]] <- c(breaks[i], breaks[i+1], breaks[i+1], breaks[i])
- }
- xaxt <- ifelse(horiz, "s", "n")
- yaxt <- ifelse(horiz, "n", "s")
- if(horiz){YLIM<-c(0,1); XLIM<-range(breaks)}
- if(!horiz){YLIM<-range(breaks); XLIM<-c(0,1)}
- if(missing(xlim)) xlim=XLIM
- if(missing(ylim)) ylim=YLIM
- plot(1,1,t="n",ylim=ylim, xlim=xlim, xaxt=xaxt, yaxt=yaxt, xaxs="i", yaxs="i", ...)  
- for(i in seq(poly)){
-  if(horiz){
-   polygon(poly[[i]], c(0,0,1,1), col=col[i], border=NA)
+image.scale <- function(
+    z, zlim, col = heat.colors(12),
+    breaks, horiz = TRUE, ylim = NULL, xlim = NULL, ...) {
+  if (!missing(breaks)) {
+    if (length(breaks) != (length(col) + 1)) {
+      stop("must have one more break than colour")
+    }
   }
-  if(!horiz){
-   polygon(c(0,0,1,1), poly[[i]], col=col[i], border=NA)
+  if (missing(breaks) & !missing(zlim)) {
+    breaks <- seq(zlim[1], zlim[2], length.out = (length(col) + 1))
   }
- }
+  if (missing(breaks) & missing(zlim)) {
+    zlim <- range(z, na.rm = TRUE)
+    zlim[2] <- zlim[2] + c(zlim[2] - zlim[1]) * (1E-3) # adds a bit to the range in both directions
+    zlim[1] <- zlim[1] - c(zlim[2] - zlim[1]) * (1E-3)
+    breaks <- seq(zlim[1], zlim[2], length.out = (length(col) + 1))
+  }
+  poly <- vector(mode = "list", length(col))
+  for (i in seq(poly)) {
+    poly[[i]] <- c(breaks[i], breaks[i + 1], breaks[i + 1], breaks[i])
+  }
+  xaxt <- ifelse(horiz, "s", "n")
+  yaxt <- ifelse(horiz, "n", "s")
+  if (horiz) {
+    YLIM <- c(0, 1)
+    XLIM <- range(breaks)
+  }
+  if (!horiz) {
+    YLIM <- range(breaks)
+    XLIM <- c(0, 1)
+  }
+  if (missing(xlim)) xlim <- XLIM
+  if (missing(ylim)) ylim <- YLIM
+  plot(1, 1, t = "n", ylim = ylim, xlim = xlim, xaxt = xaxt, yaxt = yaxt, xaxs = "i", yaxs = "i", ...)
+  for (i in seq(poly)) {
+    if (horiz) {
+      polygon(poly[[i]], c(0, 0, 1, 1), col = col[i], border = NA)
+    }
+    if (!horiz) {
+      polygon(c(0, 0, 1, 1), poly[[i]], col = col[i], border = NA)
+    }
+  }
 }
 
-color.bar <- function(colores, min, max=-min, nticks=length(colores)+1,
-	 ro=1,ticks=round(seq(min, max, len=nticks),ro),xm=NULL,ym=NULL,xw=NULL,yw=NULL,horiz=TRUE,xpos=1) {
-	xaxp=par()$usr[1:2]
-	yaxp=par()$usr[3:4]
-    nc=length(colores)
-	if (horiz & xpos==1) pos=1
-	if (horiz & xpos==0) pos=3
-	if (!horiz & xpos==1) pos=2
-	if (!horiz & xpos==0) pos=4
-	if (horiz) {
-		if (is.null(xw)) {xw=0.4*(xaxp[2]-xaxp[1])}
-		if (is.null(yw)) {yw=0.025*(yaxp[2]-yaxp[1])}
-		if (is.null(xm)) {xm=(xaxp[2]+xaxp[1])/2}
-		if (is.null(ym)) {ym=yaxp[1]+xpos*(yaxp[2]-yaxp[1])-2*(xpos-.5)*yw}
-		scale = nc/(2*xw)
+color.bar <- function(colores, min, max = -min, nticks = length(colores) + 1,
+                      ro = 1, ticks = round(seq(min, max, len = nticks), ro), xm = NULL, ym = NULL, xw = NULL, yw = NULL, horiz = TRUE, xpos = 1) {
+  xaxp <- par()$usr[1:2]
+  yaxp <- par()$usr[3:4]
+  nc <- length(colores)
+  if (horiz & xpos == 1) pos <- 1
+  if (horiz & xpos == 0) pos <- 3
+  if (!horiz & xpos == 1) pos <- 2
+  if (!horiz & xpos == 0) pos <- 4
+  if (horiz) {
+    if (is.null(xw)) {
+      xw <- 0.4 * (xaxp[2] - xaxp[1])
+    }
+    if (is.null(yw)) {
+      yw <- 0.025 * (yaxp[2] - yaxp[1])
+    }
+    if (is.null(xm)) {
+      xm <- (xaxp[2] + xaxp[1]) / 2
+    }
+    if (is.null(ym)) {
+      ym <- yaxp[1] + xpos * (yaxp[2] - yaxp[1]) - 2 * (xpos - .5) * yw
+    }
+    scale <- nc / (2 * xw)
     for (i in 1:nc) {
-	     x = xm-xw+(i-1)/scale 
-	     rect(x,ym-yw,x+1/scale,ym+yw, col=colores[i], border=NA)	
-		 text(x,ym-2*(xpos-.5)*yw,ticks[i],pos=pos,offset=0.5) 
-    		}
-		 text(xm+xw,ym-2*(xpos-.5)*yw,ticks[nc+1],pos=pos,offset=0.5)
-	} else {
-		if (is.null(xw)) {xw=0.025*(xaxp[2]-xaxp[1])}
-		if (is.null(yw)) {yw=0.4*(yaxp[2]-yaxp[1])}
-		if (is.null(xm)) {xm=xaxp[1]+xpos*(xaxp[2]-xaxp[1])-2*(xpos-.5)*xw}
-		if (is.null(ym)) {ym=(yaxp[2]+yaxp[1])/2}
-		scale = nc/(2*yw)
+      x <- xm - xw + (i - 1) / scale
+      rect(x, ym - yw, x + 1 / scale, ym + yw, col = colores[i], border = NA)
+      text(x, ym - 2 * (xpos - .5) * yw, ticks[i], pos = pos, offset = 0.5)
+    }
+    text(xm + xw, ym - 2 * (xpos - .5) * yw, ticks[nc + 1], pos = pos, offset = 0.5)
+  } else {
+    if (is.null(xw)) {
+      xw <- 0.025 * (xaxp[2] - xaxp[1])
+    }
+    if (is.null(yw)) {
+      yw <- 0.4 * (yaxp[2] - yaxp[1])
+    }
+    if (is.null(xm)) {
+      xm <- xaxp[1] + xpos * (xaxp[2] - xaxp[1]) - 2 * (xpos - .5) * xw
+    }
+    if (is.null(ym)) {
+      ym <- (yaxp[2] + yaxp[1]) / 2
+    }
+    scale <- nc / (2 * yw)
     for (i in 1:nc) {
-	     y = ym-yw+(i-1)/scale 
-	     rect(xm-xw,y,xm+xw,y+1/scale, col=colores[i], border=NA)
-		 text(xm-2*(xpos-.5)*xw,y,ticks[i],pos=pos,offset=0.5) 
-    		}
-		 text(xm-2*(xpos-.5)*xw,ym+yw,ticks[nc+1],pos=pos,offset=0.5) 
-
-		}
+      y <- ym - yw + (i - 1) / scale
+      rect(xm - xw, y, xm + xw, y + 1 / scale, col = colores[i], border = NA)
+      text(xm - 2 * (xpos - .5) * xw, y, ticks[i], pos = pos, offset = 0.5)
+    }
+    text(xm - 2 * (xpos - .5) * xw, ym + yw, ticks[nc + 1], pos = pos, offset = 0.5)
+  }
 }
